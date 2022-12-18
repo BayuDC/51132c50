bdd73d3467df3202d5018e005988e7a91eb3d871
BayuDC <dev.bayudc@gmail.com>
2022-12-18 21:23:00 +0700

    chore: control log level

--- core/db.go
+++ core/db.go
@@ -18,8 +18,14 @@ func ConnectDb() *gorm.DB {
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
 		host, username, password, database, port)
+
+	logMode := logger.Info
+	if os.Getenv("GIN_MODE") == "release" {
+		logMode = logger.Silent
+	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.Default.LogMode(logMode),
 	})
 	if err != nil {
 		panic("Failed to connect to database")
