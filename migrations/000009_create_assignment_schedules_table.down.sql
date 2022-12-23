d7a26ffa664a9c61b1ba7ebd4e35292cf470e91b
BayuDC <dev.bayudc@gmail.com>
2022-12-23 16:24:37 +0700

    fix: wrong table name

--- migrations/000009_create_assignment_schedules_table.down.sql
+++ migrations/000009_create_assignment_schedules_table.down.sql
@@ -1,3 +1,3 @@
-ALTER TABLE "assigment_schedules" DROP CONSTRAINT "assigment_schedules_assignment_id_foreign";
-ALTER TABLE "assigment_schedules" DROP CONSTRAINT "assigment_schedules_group_id_foreign";
-DROP TABLE "assigment_schedules";
+ALTER TABLE "assignment_schedules" DROP CONSTRAINT "assignment_schedules_assignment_id_foreign";
+ALTER TABLE "assignment_schedules" DROP CONSTRAINT "assignment_schedules_group_id_foreign";
+DROP TABLE "assignment_schedules";
--- migrations/000009_create_assignment_schedules_table.up.sql
+++ migrations/000009_create_assignment_schedules_table.up.sql
@@ -1,11 +1,11 @@
-CREATE TABLE "assigment_schedules"(
+CREATE TABLE "assignment_schedules"(
     "id" SERIAL NOT NULL PRIMARY KEY,
     "assignment_id" INTEGER NOT NULL,
     "group_id" INTEGER NOT NULL,
     "open_at" TIME(0) WITHOUT TIME ZONE NOT NULL,
     "close_at" TIME(0) WITHOUT TIME ZONE NOT NULL
 );
-ALTER TABLE "assigment_schedules"
-ADD CONSTRAINT "assigment_schedules_assignment_id_foreign" FOREIGN KEY("assignment_id") REFERENCES "assignments"("id") ON DELETE CASCADE;
-ALTER TABLE "assigment_schedules"
-ADD CONSTRAINT "assigment_schedules_group_id_foreign" FOREIGN KEY("group_id") REFERENCES "groups"("id") ON DELETE CASCADE;
+ALTER TABLE "assignment_schedules"
+ADD CONSTRAINT "assignment_schedules_assignment_id_foreign" FOREIGN KEY("assignment_id") REFERENCES "assignments"("id") ON DELETE CASCADE;
+ALTER TABLE "assignment_schedules"
+ADD CONSTRAINT "assignment_schedules_group_id_foreign" FOREIGN KEY("group_id") REFERENCES "groups"("id") ON DELETE CASCADE;
